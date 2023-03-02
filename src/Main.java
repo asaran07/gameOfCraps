91c1ff1c37bd9cb8112a3df318f64feaa6d9d028
Arshdeep Saran <saran.arsh07@icloud.com>
2023-03-02 09:43:42 -0800

    switch to intellij gui editor

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenHeight = screenSize.height;
-        int screenWidth = screenSize.width;
 
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-        JButton button = new JButton();
-        button.setBounds(960,540,100,40);
-        frame.add(button);
     }
 }
---
+++ src/mainForm.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class mainForm {
+    private JPanel panel1;
+    private JLabel title;
+    private JLabel version;
+}
