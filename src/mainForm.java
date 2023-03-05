ce5e83d7d06e957c50948b47168400f284341cc5
Arshdeep Saran <saran.arsh07@icloud.com>
2023-03-05 15:56:01 -0800

    switch screens version 0.1.3

---
+++ src/mainForm.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class mainForm extends JFrame {
+    private JPanel mainPanel;
+    private JPanel titleScreen;
+    private JPanel settingsScreen;
+    private JLabel titleText;
+    private JLabel settingsText;
+    private JButton settingbutton;
+    private JButton backbutton;
+
+    public mainForm() {
+        settingbutton.addActionListener(e -> switchPanel(settingsScreen));
+        backbutton.addActionListener(e -> switchPanel(titleScreen));
+    }
+
+    private void switchPanel(JPanel thePanel) {
+        mainPanel.removeAll();
+        mainPanel.add(thePanel);
+        mainPanel.repaint();
+        mainPanel.revalidate();
+    }
+
+    public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int sHeight = screenSize.height/2;
+        int sWidth = screenSize.width/2;
+        Dimension screenDimension = new Dimension(sWidth,sHeight);
+        mainForm mf = new mainForm();
+        mf.setTitle("TGOS 0.1.1");
+        mf.setContentPane(mf.mainPanel);
+        mf.setSize(screenDimension);
+        mf.setVisible(true);
+        mf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+}
